9788b12e4ce57c5426bf1c844798aa0335e5eec4
Zach Smith <zsmith126028@gmail.com>
2019-06-27 18:37:47 -0500

    create TechItem component, bring in to TechListModal

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -26,9 +26,7 @@ const TechListModal = () => {
         <h4>Technician List</h4>
         <ul className='collection'>
           {!loading &&
-            techs.map(tech => (
-              <li className='collection-item'>{tech.firstName}</li>
-            ))}
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
         </ul>
       </div>
     </div>
