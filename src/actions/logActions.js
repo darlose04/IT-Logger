ffa5f9e3314a7c14b00af3e85648036742bcdc64
Zach Smith <zsmith126028@gmail.com>
2019-07-01 11:41:53 -0500

    change error payload to statusText

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -39,7 +39,7 @@ export const getLogs = () => async dispatch => {
   } catch (error) {
     dispatch({
       type: LOGS_ERROR,
-      payload: error.response.data
+      payload: error.response.statusText
     });
   }
 };
@@ -65,7 +65,7 @@ export const addLog = log => async dispatch => {
   } catch (error) {
     dispatch({
       type: LOGS_ERROR,
-      payload: error.response.data
+      payload: error.response.statusText
     });
   }
 };
@@ -86,7 +86,7 @@ export const deleteLog = id => async dispatch => {
   } catch (error) {
     dispatch({
       type: LOGS_ERROR,
-      payload: error.response.data
+      payload: error.response.statusText
     });
   }
 };
@@ -113,7 +113,7 @@ export const updateLog = log => async dispatch => {
   } catch (error) {
     dispatch({
       type: LOGS_ERROR,
-      payload: error.response.data
+      payload: error.response.statusText
     });
   }
 };
@@ -133,7 +133,7 @@ export const searchLogs = text => async dispatch => {
   } catch (error) {
     dispatch({
       type: LOGS_ERROR,
-      payload: error.response.data
+      payload: error.response.statusText
     });
   }
 };
